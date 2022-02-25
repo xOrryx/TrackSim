f80e4bd50c56c4c730a6ccf83f20e1040538c3ae
xOrryx <xsopak00@vutbr.cz>
2022-02-25 19:00:35 +0100

    ModifiedUI
    modifie UI - can be only one on

---
+++ Tracksim/Assets/Script/OnlyOneButton.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnlyOneButton : MonoBehaviour
+{
+    [SerializeField] private Button straightButt;
+    [SerializeField] private Button curvedButt;
+    public bool status; // false - straight, true = curve
+    private ColorBlock buttonColor;
+  
+    public void ChangeButtonStraight()
+    {
+        buttonColor = straightButt.colors;
+        buttonColor.normalColor = new Color32(255, 128, 128, 255);
+        straightButt.colors = buttonColor;
+
+        status = false;
+
+        buttonColor = curvedButt.colors;
+        buttonColor.normalColor = new Color32(255, 255, 255, 255);
+        curvedButt.colors = buttonColor;
+    }
+
+    public void ChangeButtonCurve()
+    {
+        buttonColor = curvedButt.colors;
+        buttonColor.normalColor = new Color32(255, 128, 128, 255);
+        curvedButt.colors = buttonColor;
+
+        status = true;
+
+        buttonColor = straightButt.colors;
+        buttonColor.normalColor = new Color32(255, 255, 255, 255);
+        straightButt.colors = buttonColor;
+    }
+}
+
