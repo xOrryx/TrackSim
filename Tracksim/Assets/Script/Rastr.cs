7f7dfa317e76434bc624fae41d0863da4c9b3fd6
xOrryx <xsopak00@vutbr.cz>
2022-04-01 02:35:37 +0200

    UI changed

--- Tracksim/Assets/Script/Rastr.cs
+++ Tracksim/Assets/Script/Rastr.cs
@@ -1,15 +1,19 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Rastr : MonoBehaviour
 {
     [SerializeField] private Plane mainPlane;
     [SerializeField] private Material rastrMaterial;
+    [SerializeField] private InputField field;
     public int distance = 0;
+
     private Vector3 startPoint = new Vector3(0, 0.05f, 0);
     private Vector3[] verticalLine;
     private Vector3[] horizontalLine;
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,30 +22,45 @@ public class Rastr : MonoBehaviour
         // Plane, which was used, doesnt have Transform API. It's not possible to get its sizes. I managed it by measuring it
         verticalLine = new Vector3[] { new Vector3(0, 0.05f, -15), startPoint, new Vector3(0, 0.05f, 35) };
         horizontalLine = new Vector3[] { new Vector3(-10, 0.05f, 0), startPoint, new Vector3(40, 0.05f, 0) };
+
         CreateRastr();
 
     }
 
+    /// <summary>
+    /// event system from UI to get value
+    /// </summary>
+    public void getDistance()
+    {       
+        distance = int.Parse(field.text);
+        //reset lines
+        ResetLines();
+        // create new rastr
+        CreateRastr();
+    }
 
+    /// <summary>
+    /// main method
+    /// </summary>
     public void CreateRastr()
     {
         // create vertical line origin
-        CreateVertLine(verticalLine, "vertical");
+        CreateLine(verticalLine, "vertical");
         // create horizontal line origin
-        CreateVertLine(horizontalLine, "horizontal");
+        CreateLine(horizontalLine, "horizontal");
         // create same lines
         CreateCopies();
     }
 
 
     /// <summary>
-    /// Creates origin line
+    /// Creates vertices lines
     /// </summary>
-    private void CreateVertLine(Vector3[] array, string name)
+    private void CreateLine(Vector3[] array, string name)
     {
         GameObject emptyObject = new GameObject();
         emptyObject.AddComponent<LineRenderer>();
-        emptyObject.transform.name = "origin - " + name ;
+        emptyObject.transform.name = "lane - " + name ;
         emptyObject.transform.localPosition = new Vector3(0, 0, 0);
 
         LineRenderer lineRen = emptyObject.GetComponent<LineRenderer>();
@@ -60,8 +79,11 @@ public class Rastr : MonoBehaviour
     /// </summary>
     private void CreateCopies()
     {
-        GameObject tempVert = GameObject.Find("origin - vertical");
-        GameObject tempHori = GameObject.Find("origin - horizontal");
+        Vector3[] tempArrayV = new Vector3[verticalLine.Length];
+        Vector3[] tempArrayH = new Vector3[horizontalLine.Length];
+
+        Array.Copy(verticalLine, tempArrayV, verticalLine.Length);
+        Array.Copy(horizontalLine, tempArrayH, verticalLine.Length);
 
         int counter = 0;
 
@@ -74,20 +96,11 @@ public class Rastr : MonoBehaviour
         {
             if(i != 0)
             {
-                GameObject newLine = GameObject.Instantiate(tempVert, new Vector3(i * distance, 0.05f, 0), new Quaternion(0,0,0,0));
-                newLine.transform.name = "lineV" + (counter).ToString();
+                for (int j = 0; j < 3; ++j)
+                    tempArrayV[j].x = i * distance;
 
-                LineRenderer newLineRen =  newLine.GetComponent<LineRenderer>();
+                CreateLine(tempArrayV, counter.ToString());
 
-                Vector3[] tempArray = verticalLine;
-
-                for(int j = 0; j < 3; ++j)
-                    tempArray[j].x = i * distance;
-                
-                newLineRen.positionCount = 3;
-                newLineRen.SetPositions(tempArray);
-
-                newLine.transform.SetParent(this.transform);
                 counter++;
             }
         }
@@ -96,24 +109,21 @@ public class Rastr : MonoBehaviour
         {
             if (i != 0)
             {
-                GameObject newLine = GameObject.Instantiate(tempVert, new Vector3(0, 0.05f, i * distance), new Quaternion(0, 0, 0, 0));
-                newLine.transform.name = "lineH" + (counter).ToString();
-
-                LineRenderer newLineRen = newLine.GetComponent<LineRenderer>();
-
-                Vector3[] tempArray = horizontalLine;
-
                 for (int j = 0; j < 3; ++j)
-                    tempArray[j].z = i * distance;
+                    tempArrayH[j].z = i * distance;
 
-                newLineRen.positionCount = 3;
-                newLineRen.SetPositions(tempArray);
+                CreateLine(tempArrayH, counter.ToString());
 
-                newLine.transform.SetParent(this.transform);
                 counter++;
             }
         }
     }
-    
 
+    private void ResetLines()
+    {
+        foreach (Transform child in this.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+    }
 }
