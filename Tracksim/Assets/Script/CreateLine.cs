076c40b2eb0a14aa6c7efd8379c3538464f2fb02
xOrryx <xsopak00@vutbr.cz>
2022-04-14 23:22:40 +0200

    add TriLine straight + regions

--- Tracksim/Assets/Script/CreateLine.cs
+++ Tracksim/Assets/Script/CreateLine.cs
@@ -60,6 +60,8 @@ public class CreateLine : MonoBehaviour
         }
     }
 
+    #region Public methods
+
     public void LeftMouseClick(Ray ray, RaycastHit[] hits)
     {
 
@@ -72,6 +74,12 @@ public class CreateLine : MonoBehaviour
             CreateLineRender();
         }
     }
+
+    #endregion
+
+    #region Private methods
+
+    #region Renderer
 
     private void CreateCube()
     {
@@ -107,16 +115,59 @@ public class CreateLine : MonoBehaviour
         }
         else if(buttonStatus.typeLine == Status.tri) // tri Lines
         {
-            usedTri = true;
-            makeTriLines();
-            DrawLine();
-            DrawTriLines();
+            if(TriLineShape == 0) //straight
+            {
+                usedTri = true;
+                makeTriLines();
+                DrawLine();
+                DrawTriLines();
+            }
+            else if(TriLineShape == 1) // curve
+            {
+                switch (TriLineType)
+                {
+                    case 0: // one side -  left
+                        break;
+                    case 1: // one side -  right
+                        break;
+                    case 2: // both side
+                        break;
+                    default: // one side -  left
+                        break;
+                }
+            }
+            else
+            {
+                Debug.LogError("Dropdown input problem");
+            }
         }
-        else // new Line
+        else
         {
+            Debug.LogError("Button Status is wrong");
+        }
+    }
 
+    #endregion
+
+    /// <summary>
+    /// Calculate curve between two points
+    /// </summary>
+    /// 
+    private void MakeCurve()
+    {
+        for (float ratio = 0; ratio <= 1; ratio += 1.0f / vertexCount)
+        {
+            var tangentLineVertex1 = Vector3.Lerp(points[points.Count - 3], points[points.Count - 2], ratio);
+            var tangentLineVertex2 = Vector3.Lerp(points[points.Count - 2], points[points.Count - 1], ratio);
+            var bezierpoint = Vector3.Lerp(tangentLineVertex1, tangentLineVertex2, ratio);
+            pointList.Add(bezierpoint);
         }
 
+        for (int i = 0; i < pointList.Count; i++)
+        {
+            points.Add(pointList[i]);
+        }
+        pointList.Clear();
     }
 
     /// <summary>
@@ -124,17 +175,101 @@ public class CreateLine : MonoBehaviour
     /// </summary>
     private void makeTriLines()
     {
-        Debug.Log(points.Count);
-
         Vector3 pointStart = points[points.Count - 2]; // penultimate point
         Vector3 pointEnd = points[points.Count-1];
-
-        Debug.Log(pointStart + " " + pointEnd);
 
         Vector3 normal = (pointEnd - pointStart);
         normal /= normal.z;
 
-        if (normal != Vector3.forward && normal != Vector3.back)
+        float angle1 = Vector3.Angle(normal, Vector3.forward);
+        float angle2 = Vector3.Angle(normal, Vector3.back);
+
+
+        switch (TriLineType)
+        {
+            case 0: // one side -  left
+                addLeftSide(pointStart, pointEnd, angle1, angle2);
+                break;
+            case 1: // one side -  right
+                addRightSide(pointStart, pointEnd, angle1, angle2);
+                break;
+            case 2: // both side
+                addBothSide(pointStart, pointEnd, angle1, angle2);
+                break;
+            default: // one side -  left
+                addLeftSide(pointStart, pointEnd, angle1, angle2);
+                break;
+        }
+
+    }
+
+    #region Calculate TriLine straight methods
+    /// <summary>
+    /// add points about right side lines (straight) to buffer
+    /// </summary>
+    /// <param name="pointStart"></param>
+    /// <param name="pointEnd"></param>
+    /// <param name="angle1"></param>
+    /// <param name="angle2"></param>
+    private void addRightSide(Vector3 pointStart, Vector3 pointEnd, float angle1, float angle2)
+    {
+        if ((angle1 > 55 && angle1 < 125 || angle2 > 55 && angle2 < 125))
+        {
+            // create upper parallel
+            pointStart.z -= 1;
+            triPoints.Add(pointStart);
+            pointEnd.z -= 1;
+            triPoints.Add(pointEnd);
+
+        }
+        else
+        {
+            // create left parallel
+            pointStart.x += 1;
+            triPoints.Add(pointStart);
+            pointEnd.x += 1;
+            triPoints.Add(pointEnd);
+        }
+    }
+
+    /// <summary>
+    /// add points about left side lines (straight) to buffer
+    /// </summary>
+    /// <param name="pointStart"></param>
+    /// <param name="pointEnd"></param>
+    /// <param name="angle1"></param>
+    /// <param name="angle2"></param>
+    private void addLeftSide(Vector3 pointStart, Vector3 pointEnd, float angle1, float angle2)
+    {
+        if ((angle1 > 55 && angle1 < 125 || angle2 > 55 && angle2 < 125))
+        {
+            // create upper parallel
+            pointStart.z += 1;
+            triPoints.Add(pointStart);
+            pointEnd.z += 1;
+            triPoints.Add(pointEnd);
+
+        }
+        else
+        {
+            // create left parallel
+            pointStart.x -= 1;
+            triPoints.Add(pointStart);
+            pointEnd.x -= 1;
+            triPoints.Add(pointEnd);
+        }
+    }
+
+    /// <summary>
+    /// add points about Both side lines (straight) to buffer
+    /// </summary>
+    /// <param name="pointStart"></param>
+    /// <param name="pointEnd"></param>
+    /// <param name="angle1"></param>
+    /// <param name="angle2"></param>
+    private void addBothSide(Vector3 pointStart, Vector3 pointEnd, float angle1, float angle2)
+    {
+        if ((angle1 > 55 && angle1 < 125 || angle2 > 55 && angle2 < 125))
         {
             // create upper parallel
             pointStart.z -= 1;
@@ -150,40 +285,27 @@ public class CreateLine : MonoBehaviour
         }
         else
         {
-            // create upper parallel
+            // create right parallel
             pointStart.x -= 1;
             triPoints.Add(pointStart);
             pointEnd.x -= 1;
             triPoints.Add(pointEnd);
 
-            // create lower parallel
-            pointStart.x += 1;
+            // create left parallel
+            pointStart.x += 2;
             triPoints.Add(pointStart);
-            pointEnd.x += 1;
+            pointEnd.x += 2;
             triPoints.Add(pointEnd);
         }
-
     }
 
-    /// <summary>
-    /// Calculate curve between two points
-    /// </summary>
-    private void MakeCurve()
-    {
-        for (float ratio = 0; ratio <= 1; ratio += 1.0f / vertexCount)
-        {
-            var tangentLineVertex1 = Vector3.Lerp(points[points.Count - 3], points[points.Count - 2], ratio);
-            var tangentLineVertex2 = Vector3.Lerp(points[points.Count - 2], points[points.Count - 1], ratio);
-            var bezierpoint = Vector3.Lerp(tangentLineVertex1, tangentLineVertex2, ratio);
-            pointList.Add(bezierpoint);
-        }
+    #endregion
 
-        for(int i = 0; i < pointList.Count; i++)
-        {
-            points.Add(pointList[i]);
-        }
-        pointList.Clear();  
-    }
+    #region Calculate TriLine curve methods
+
+    #endregion
+
+    #region Draw Line
 
     private void DrawLine()
     {
@@ -221,11 +343,15 @@ public class CreateLine : MonoBehaviour
             lineRenObj.SetPositions(new Vector3[] { triTemp1, triTemp2 });
 
             lineRenObj.material = lineMaterial;
-            lineRenObj.startWidth = 0.25f;
-            lineRenObj.endWidth = 0.25f;
+            lineRenObj.startWidth = 0.3f;
+            lineRenObj.endWidth = 0.3f;
 
         }
     }
+
+    #endregion
+
+    #region Reset Lines
 
     private void ResetLine()
     {
@@ -240,4 +366,7 @@ public class CreateLine : MonoBehaviour
         }
     }
 
+    #endregion
+
+    #endregion
 }
